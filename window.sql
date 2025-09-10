c971b8e1ec037beb7326ca085aaaa6a860072d3e
tanishsharma123 <tanish829017@gmail.com>
2025-09-10 10:33:50 +0530

    Add files via upload
    window

---
+++ window.sql
@@ -0,0 +1,73 @@
+create database tushar;
+use tushar;
+CREATE TABLE EmployeeSales (
+    EmployeeID INT,
+    EmployeeName VARCHAR(100),
+    Department VARCHAR(50),
+    SaleDate DATE,
+    SaleAmount DECIMAL(10, 2)
+);
+
+
+INSERT INTO EmployeeSales (EmployeeID, EmployeeName, Department, SaleDate, SaleAmount) VALUES
+(1, 'Alice', 'Sales', '2025-06-01', 1200.00),
+(1, 'Alice', 'Sales', '2025-06-03', 1800.00),
+(1, 'Alice', 'Sales', '2025-06-07', 1500.00),
+(2, 'Bob', 'Sales', '2025-06-01', 2000.00),
+(2, 'Bob', 'Sales', '2025-06-05', 2200.00),
+(2, 'Bob', 'Sales', '2025-06-08', 2100.00),
+(3, 'Carol', 'Marketing', '2025-06-02', 3000.00),
+(3, 'Carol', 'Marketing', '2025-06-06', 2800.00),
+(3, 'Carol', 'Marketing', '2025-06-10', 2700.00),
+(4, 'Dave', 'Marketing', '2025-06-01', 1000.00),
+(4, 'Dave', 'Marketing', '2025-06-03', 1100.00),
+(4, 'Dave', 'Marketing', '2025-06-09', 1300.00),
+(5, 'Eve', 'HR', '2025-06-04', 900.00),
+(5, 'Eve', 'HR', '2025-06-07', 950.00),
+(5, 'Eve', 'HR', '2025-06-10', 1000.00),
+(6, 'Frank', 'Sales', '2025-06-02', 1700.00),
+(6, 'Frank', 'Sales', '2025-06-05', 1900.00),
+(6, 'Frank', 'Sales', '2025-06-09', 1600.00),
+(7, 'Grace', 'Finance', '2025-06-01', 2500.00),
+(7, 'Grace', 'Finance', '2025-06-06', 2400.00),
+(7, 'Grace', 'Finance', '2025-06-10', 2600.00),
+(8, 'Hank', 'Finance', '2025-06-03', 2300.00),
+(8, 'Hank', 'Finance', '2025-06-04', 2200.00),
+(8, 'Hank', 'Finance', '2025-06-08', 2100.00),
+(9, 'Ivy', 'HR', '2025-06-02', 800.00),
+(9, 'Ivy', 'HR', '2025-06-06', 850.00),
+(9, 'Ivy', 'HR', '2025-06-09', 950.00),
+(10, 'Jake', 'IT', '2025-06-05', 3000.00),
+(10, 'Jake', 'IT', '2025-06-07', 3200.00),
+(10, 'Jake', 'IT', '2025-06-10', 3100.00);
+
+select * from employeeSales;
+
+select avg(saleamount) from employeesales;  -- 1891.666667
+
+select *, (select avg(saleamount) from employeesales) from employeesales
+group by department;  -- 1891.666667
+
+select *,
+avg(saleamount) over(),
+max(saleamount) over(),
+min(saleamount) over() from employeesales;
+
+select *, avg(saleamount) over(),
+avg(saleamount) over(partition by department)
+from employeesales;
+
+select *, avg(saleamount) over(),
+avg(saleamount) over(partition by saledate)
+from employeesales;
+
+select *, avg(saleamount) over(partition by department),
+avg(saleamount) over(partition by department, employeename)
+from employeesales;
+
+select *, avg(saleamount) over(partition by department),
+avg(saleamount) over(partition by department, saledate)
+from employeesales;
+
+select *,
+sum(saleamount) over( partition by department order by saleamount) from employeesales;
